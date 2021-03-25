1d431c68ee38aeb11cac4f99bc503577155a3eac
cxkaha <kaj.hammarberg@tuni.fi>
2021-03-25 15:28:49 +0200

    pitkäpainallus kopioi tekstin ominaisuus

--- app/src/main/java/com/example/mlmuistikirja/ReadActivity.kt
+++ app/src/main/java/com/example/mlmuistikirja/ReadActivity.kt
@@ -1,8 +1,12 @@
 package com.example.mlmuistikirja
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 
 class ReadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +17,21 @@ class ReadActivity : AppCompatActivity() {
 
         if(intent.extras != null) {
             textReader.text = intent.getStringExtra("EXTRA_TEXT")
+        }
+
+        // Pitkäpainallus listener jonka avulla kopioidaan teksti leikepöydälle
+        textReader.setOnLongClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("teksti", textReader!!.text)
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(
+                applicationContext,
+                "Teksti kopioitu",
+                Toast.LENGTH_LONG
+            ).show()
+
+            return@setOnLongClickListener true
         }
     }
 }
