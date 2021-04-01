d254bed7c0528f092abb8954ef9605bae4657f27
cxkaha <kaj.hammarberg@tuni.fi>
2021-04-01 14:29:41 +0300

    turha dependency poistettu

--- app/src/main/java/com/example/mlmuistikirja/CameraActivity.kt
+++ app/src/main/java/com/example/mlmuistikirja/CameraActivity.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.AlertDialog
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaActionSound
